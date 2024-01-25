bd55a5ef5caa4cc60a19d19327a2d87fb56bcf3f
Natsuki Oshio <natsuki.oshio@rakus-partners.co.jp>
2024-01-25 13:34:32 +0900

    addファイル

---
+++ src/main/java/com/example/practice1/repractices/Ex39.java
@@ -0,0 +1,11 @@
+package com.example.practice1.repractices;
+
+/**
+ * 演習３９
+ * フィボナッチ数列を表示させるプログラムを作成してください
+ * 結果が1000を超えた時点で終了してください
+ * 1 1 2 3 5 8....
+ */
+public class Ex39 {
+    
+}
---
+++ src/main/java/com/example/practice1/repractices/ex31.java
@@ -0,0 +1,13 @@
+package com.example.practice1.repractices;
+
+/** 
+ * 演習３１
+ * for文を使用して、１から１０までの総和を表示してください
+ */
+public class ex31 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
+
